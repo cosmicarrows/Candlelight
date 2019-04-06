afe67b03b650ad0c9662abed91cdce1c432d3bd1
LaurenceW <laurence@cosmicarrows.com>
2019-04-06 00:11:49 -0400

    Animate the Select Whip UIButton and a Add Drop Shadow

--- Candlelight/View/RoundedShadowButton.swift
+++ Candlelight/View/RoundedShadowButton.swift
@@ -9,7 +9,58 @@
 import UIKit
 
 class RoundedShadowButton: UIButton {
-
     
+    var originalSize: CGRect?
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+
+    func setupView(){
+        originalSize = self.frame
+        self.layer.cornerRadius = 5.0
+        self.layer.shadowRadius = 10.0
+        self.layer.shadowColor = UIColor.cyan.cgColor
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowOffset = CGSize.zero
+    }
+    
+    func animateButton(shouldLoad: Bool, withMessage message: String?){
+        let spinner = UIActivityIndicatorView.init()
+        spinner.style = .whiteLarge
+        spinner.color = UIColor.cyan
+        spinner.alpha = 0.0
+        spinner.hidesWhenStopped = true
+        spinner.tag = 96
+        if shouldLoad{
+            self.addSubview(spinner)
+            self.setTitle("", for: .normal)
+            UIView.animate(withDuration: 0.2, animations: {
+                self.layer.cornerRadius = self.frame.height / 2
+                self.frame = CGRect.init(x: self.frame.midX - (self.frame.height / 2), y: self.frame.origin.y, width: self.frame.height, height: self.frame.height)
+            }) { (finished) in
+                if finished == true {
+                    spinner.startAnimating()
+                    spinner.center = CGPoint.init(x: self.frame.width / 2, y: self.frame.width / 2 + 1)
+                    UIView.animate(withDuration: 0.2, animations: {
+                        spinner.alpha = 1.0
+                    })
+                }
+            }
+            self.isUserInteractionEnabled = false
+        } else {
+            self.isUserInteractionEnabled = true
+            for subview in self.subviews {
+                if subview.tag == 96 {
+                    subview.removeFromSuperview()
+                }
+            }
+            UIView.animate(withDuration: 0.2) {
+                self.layer.cornerRadius = 5.0
+                self.frame = self.originalSize!
+                self.setTitle(message, for: .normal)
+            }
+        }
+    }
 
 }
