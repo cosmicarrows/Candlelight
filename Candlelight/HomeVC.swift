84efb34810f1784cada681fc58fbb1d440b71ac5
LaurenceW <laurence@cosmicarrows.com>
2019-04-25 23:00:00 -0400

    Add blue flame animation during app launch

--- Candlelight/HomeVC.swift
+++ Candlelight/HomeVC.swift
@@ -8,19 +8,28 @@
 
 import UIKit
 import MapKit
+import RevealingSplashView
 
 class HomeVC: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    
+    
     @IBOutlet weak var actionButton: RoundedShadowButton!
     
     var delegate: CenterVCDelegate?
     
+    let revealingSplashView = RevealingSplashView.init(iconImage: UIImage.init(named: "blueFlame")!, iconInitialSize: CGSize.init(width: 80, height: 80), backgroundColor: UIColor.white)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        self.view.addSubview(revealingSplashView)
+        revealingSplashView.animationType = .popAndZoomOut
+        revealingSplashView.startAnimation()
         
+        revealingSplashView.heartAttack = true
     }
 
     @IBAction func actionButtonWasPressed(_ sender: Any) {
